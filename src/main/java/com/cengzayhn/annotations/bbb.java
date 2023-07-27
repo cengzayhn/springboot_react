b725fa6d4cf8eb138db08b531f555c8ac59f8e4b
cengzayhn <mcengzayhn@gmail.com>
2023-07-27 18:06:34 +0300

    new branch updates

---
+++ src/main/java/com/cengzayhn/annotations/bbb.java
@@ -0,0 +1,7 @@
+package com.cengzayhn.annotations;
+
+public class bbb {
+    public static void main(String[] args) {
+        System.out.println("new branch new class");
+    }
+}
---
+++ src/main/java/com/cengzayhn/annotations/ccc.java
@@ -0,0 +1,7 @@
+package com.cengzayhn.annotations;
+
+public class ccc {
+    public static void main(String[] args) {
+        System.out.println("new branch new class");
+    }
+}
